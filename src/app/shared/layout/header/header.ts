76c4b20ef682b0864588061100466902cf27b39c
Phil BStorm <philippe.haerens@bstorm.be>
2026-03-11 10:30:46 +0100

    [Add] Premiers composant (header & nav-bar)

---
+++ src/app/shared/layout/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavBar } from '../nav-bar/nav-bar';
+
+@Component({
+  selector: 'app-header',
+  imports: [NavBar],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {}
