6f423123189857a25780d394fbbe91b7dc81596f
Phil BStorm <philippe.haerens@bstorm.be>
2026-03-20 11:18:38 +0100

    Demo formulaire

--- src/app/features/auth/pages/auth-login-page/auth-login-page.ts
+++ src/app/features/auth/pages/auth-login-page/auth-login-page.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+  ɵInternalFormsSharedModule,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { FormsErrorDisplay } from '@shared/components/forms-error-display/forms-error-display';
 
 @Component({
   selector: 'app-auth-login-page',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule, FormsErrorDisplay, NgClass],
   templateUrl: './auth-login-page.html',
   styleUrl: './auth-login-page.scss',
 })
 export class AuthLoginPage {
+  private readonly _fb = inject(FormBuilder);
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+
+  formLogin = this._fb.group({
+    email: this.email,
+    password: this.password,
+  });
+
+  onSubmit() {
+    this.formLogin.markAllAsTouched();
+
+    if (this.formLogin.valid) {
+      console.log(this.formLogin.value);
+    }
+  }
 }
