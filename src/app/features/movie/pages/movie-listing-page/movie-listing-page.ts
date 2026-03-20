7ef2fe9055528b60f9684f25f88cb6de37bdd8d5
Phil BStorm <philippe.haerens@bstorm.be>
2026-03-20 13:32:29 +0100

    Demo  QueryParam

--- src/app/features/movie/pages/movie-listing-page/movie-listing-page.ts
+++ src/app/features/movie/pages/movie-listing-page/movie-listing-page.ts
@@ -1,17 +1,43 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MovieCard } from '../../../../shared/components/movie-card/movie-card';
 import { MovieService } from '../../../../core/services/movie.service';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-listing-page',
-  imports: [MovieCard],
+  imports: [MovieCard, FormsModule],
   templateUrl: './movie-listing-page.html',
   styleUrl: './movie-listing-page.scss',
 })
-export class MovieListingPage {
+export class MovieListingPage implements OnInit {
   private readonly _movieService = inject(MovieService);
+  private readonly _router = inject(Router);
+  private readonly _activatedRoute = inject(ActivatedRoute);
 
   movies = this._movieService.movieList;
+
+  /* Chmaps de recherche */
+  movieTitleSearch = '';
+
+  ngOnInit(): void {
+    this._activatedRoute.queryParams.subscribe({
+      next: (queryParam) => {
+        console.log(queryParam);
+        this.movieTitleSearch = queryParam['movieTitle'];
+      },
+    });
+  }
+
+  onClickSearch() {
+    // appel API
+
+    this._router.navigate(['/'], {
+      queryParams: {
+        movieTitle: this.movieTitleSearch,
+      },
+    });
+  }
 
   onLikeEvent(movieId: number) {
     this._movieService.likeMovie(movieId);
