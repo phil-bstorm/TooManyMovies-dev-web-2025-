106f4a2fc5d7c87d47d75245630285096c4db98a
Phil BStorm <philippe.haerens@bstorm.be>
2026-03-20 15:36:29 +0100

    Page 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MovieListingPage } from './features/movie/pages/movie-listing-page/movie-listing-page';
 import { connectedGuard } from '@core/guards/connected.guard';
 import { adminGuard } from '@core/guards/admin.guard';
+import { Error404Page } from './features/errors/pages/error-404-page/error-404-page';
 
 export const routes: Routes = [
   {
@@ -33,5 +34,9 @@ export const routes: Routes = [
     canActivate: [connectedGuard],
     loadChildren: () =>
       import('./features/user/user.routes').then((r) => r.routes),
+  },
+  {
+    path: '**',
+    component: Error404Page,
   },
 ];
---
+++ src/app/features/errors/pages/error-404-page/error-404-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-error-404-page',
+  imports: [RouterLink],
+  templateUrl: './error-404-page.html',
+  styleUrl: './error-404-page.scss',
+})
+export class Error404Page {}
