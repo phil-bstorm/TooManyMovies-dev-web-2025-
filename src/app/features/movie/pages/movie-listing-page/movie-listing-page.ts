d52b4d1f00efcd1634f6f718eab897ffcd9a32f8
Phil BStorm <philippe.haerens@bstorm.be>
2026-03-23 09:17:01 +0100

    WIP demo HttpClient

--- src/app/features/movie/pages/movie-listing-page/movie-listing-page.ts
+++ src/app/features/movie/pages/movie-listing-page/movie-listing-page.ts
@@ -1,8 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MovieCard } from '../../../../shared/components/movie-card/movie-card';
 import { MovieService } from '../../../../core/services/movie.service';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Movie } from '@core/models/movie.interface';
 
 @Component({
   selector: 'app-movie-listing-page',
@@ -15,23 +16,44 @@ export class MovieListingPage implements OnInit {
   private readonly _router = inject(Router);
   private readonly _activatedRoute = inject(ActivatedRoute);
 
-  movies = this._movieService.movieList;
+  movies = signal<Array<Movie>>([]);
+  moviesError = signal<string>('');
 
   /* Chmaps de recherche */
   movieTitleSearch = '';
 
   ngOnInit(): void {
     this._activatedRoute.queryParams.subscribe({
-      next: (queryParam) => {
+      next: async (queryParam) => {
         console.log(queryParam);
         this.movieTitleSearch = queryParam['movieTitle'];
+
+        // OBSERVABLE
+        this._movieService.getMoviesObservable().subscribe({
+          next: (response) => {
+            console.log(response);
+
+            this.movies.set(response);
+          },
+          error: (err: Error) => {
+            // Une erreur s'est produite
+            this.moviesError.set(err.message);
+          },
+        });
+
+        // PROMESSE
+        // try {
+        //   this.movies.set(await this._movieService.getMoviesPromise());
+        // } catch (error) {
+        //   const e = error as Error;
+        //   console.log(e);
+        //   this.moviesError.set(e.message);
+        // }
       },
     });
   }
 
   onClickSearch() {
-    // appel API
-
     this._router.navigate(['/'], {
       queryParams: {
         movieTitle: this.movieTitleSearch,
